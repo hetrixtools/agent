24695c687a902c938d76809287df8f2c4e78087f
HetrixTools <admin@hetrixtools.com>
2017-07-22 15:44:47 +0300

    improved network interface detection

--- hetrixtools_install.sh
+++ hetrixtools_install.sh
@@ -80,7 +80,7 @@ echo "... done."
 
 # Finding the public network interface name, based on your public IP address
 echo "Finding your public network interface name..."
-NetworkInterface=$(ip route get 8.8.8.8 | awk '{ print $(NF-2); exit }')
+NetworkInterface=$(ip route get 8.8.8.8 | awk '{ print $5; exit }')
 # Fallback on eth0 if couldn't find interface name
 if [ -z "$NetworkInterface" ]
 then
