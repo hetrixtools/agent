36eba2f72aebce563015062cf677c630985bf390
HetrixTools <admin@hetrixtools.com>
2018-12-11 17:51:24 +0200

    1.5.4
    Changed API endpoint domain name.

--- hetrixtools_uninstall.sh
+++ hetrixtools_uninstall.sh
@@ -79,7 +79,7 @@ echo "... done."
 # Let HetrixTools platform know uninstall has been completed
 echo "Letting HetrixTools platform know the uninstallation has been completed..."
 POST="v=uninstall&s=$SID"
-wget -t 1 -T 30 -qO- --post-data "$POST" --no-check-certificate https://sm.hetrixtools.com/ &> /dev/null
+wget -t 1 -T 30 -qO- --post-data "$POST" --no-check-certificate https://sm.hetrixtools.net/ &> /dev/null
 echo "... done."
 
 # All done
