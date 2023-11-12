91a5d638865183d37fa0f27cdb6f748dc9c5a820
Ry3nlNaToR <49639150+Ry3nlNaToR@users.noreply.github.com>
2023-11-12 00:52:01 +0000

    Replace ifconfig with ip command for IP address extraction

--- hetrixtools_agent.sh
+++ hetrixtools_agent.sh
@@ -478,8 +478,8 @@ do
 	TX=$(echo "$TX" | awk '{printf "%18.0f",$1}' | xargs)
 	NICS="$NICS$NIC,$RX,$TX;"
 	# Individual NIC IP addresses
-	IPv4="$IPv4$NIC,$(ifconfig "$NIC" | grep -w 'inet' | awk '{print $2}' | xargs | sed 's/ /,/g');"
-	IPv6="$IPv6$NIC,$(ifconfig "$NIC" | grep -w 'inet6' | grep -w 'global' | awk '{print $2}' | xargs | sed 's/ /,/g');"
+	IPv4="$IPv4$NIC,$(ip -4 addr show "$NIC" | grep -oP 'inet \K[\d.]+' | xargs | sed 's/ /,/g')"
+	IPv6="$IPv6$NIC,$(ip -6 addr show "$NIC" | grep -oP 'inet6 \K[0-9a-fA-F:]+' | xargs | sed 's/ /,/g')"
 done
 NICS=$(echo -ne "$NICS" | base64 | xargs | sed 's/ //g')
 IPv4=$(echo -ne "$IPv4" | base64 | xargs | sed 's/ //g')
