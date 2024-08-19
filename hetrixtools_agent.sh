3f932d5e154a7ce90589dbb885983a8832e38b3f
HetrixTools <admin@hetrixtools.com>
2024-08-19 19:08:24 +0300

    2.2.5
    - fixed typo

--- hetrixtools_agent.sh
+++ hetrixtools_agent.sh
@@ -647,7 +647,7 @@ then
 				zpoolstatus=$(echo -ne "$zpoolstatus" | base64 | tr -d '\n\r\t ')
 				mnt=$(echo -ne "$dfPB1" | grep "$i " | awk '{print $(NF)}')
 				ZP="$ZP$mnt,$i,$zpoolstatus;"
-				zpooldiskusage[$mnt]=$(zfs get -H -o value -p used,avail rpool | xargs | awk '{printf "%.0f %.0f %.0f", $1+$2, $1, $2}')
+				zpooldiskusage[$mnt]=$(zfs get -H -o value -p used,avail "$i" | xargs | awk '{printf "%.0f %.0f %.0f", $1+$2, $1, $2}')
 			done
 		fi
 	fi
